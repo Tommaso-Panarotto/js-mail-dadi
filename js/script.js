b7313e5ef72cda2e041404498dda2bdd2998030d
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-08-19 14:45:47 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*
+ESERCIZIO 1 - Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Stampiamo in pagina i due tiri e il risultato
+*/
+
+console.log('JS OK');
